9a00efdfd06a57b49c174d4c0c5bed2e7afc1869
Kishore Nallan <kishore@kishorelive.com>
2012-03-18 14:51:54 +0530

    Deprecate use of keyword 'map' as the first element in route mapping array.
    A route mapping is now simply defined as:
    
    module.exports = [
        ['get', '/customPath', 'controllerName', 'methodName'],
    ]

--- example/routes.js
+++ example/routes.js
@@ -1,6 +1,6 @@
 module.exports = [
 	// custom routes - mapping URLs to specific controller methods
-	['map', 'get', '/customPath', 'index', 'custom'],
-	['map', 'post', '/customPath/:id', 'index', 'custom'],
-	['map', 'get', '/customPath/:id/latest', 'index', 'customLatest']
+	['get', '/customPath', 'index', 'custom'],
+	['post', '/customPath/:id', 'index', 'custom'],
+	['get', '/customPath/:id/latest', 'index', 'customLatest']
 ];
